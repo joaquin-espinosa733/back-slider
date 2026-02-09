ee99d73347c2c790b7598abc2839fd0c86e3e1df
joaquin <juaco.enero9@gmail.com>
2026-02-09 11:30:06 -0300

    nueva url mongo

--- src/db/db.js
+++ src/db/db.js
@@ -5,7 +5,10 @@ dotenv.config(); // Cargar variables de entorno
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Conectado a MongoDB");
   } catch (error) {
     console.error("Error conectando a MongoDB:", error);
--- src/routes/uploadRoutes.js
+++ src/routes/uploadRoutes.js
@@ -3,7 +3,11 @@ const uploadRouter = express.Router();
 const { uploadVideo, confirmUpload } = require("../controllers/uploadController");
 const upload = require("../middlewares/upload");
 
-uploadRouter.post("/upload", uploadVideo);
+uploadRouter.post(
+    "/upload",
+    upload.single("video"),
+    uploadVideo
+);
 uploadRouter.post("/confirm", confirmUpload);
 
 
